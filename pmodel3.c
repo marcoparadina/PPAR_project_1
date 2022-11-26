53358720e9eda3e85c097c8f1eb665f70fedafa0
marcoparadina <mparadina10@gmail.com>
2022-11-26 18:47:31 +0100

    distribution of A does work kill me pls nightmarenightmarenightmare

--- pmodel3.c
+++ pmodel3.c
@@ -80,7 +80,8 @@ void process_command_line_options(int argc, char ** argv)
 //      to be replaced with a b_distr
 int main(int argc, char ** argv){
 
-    MPI_Init(NULL, NULL);
+    MPI_Init(NULL, NULL); //If Cblacs_pinfo is called, MPI_Init is called by it as well if it hasn't been called yet. Since here we never use Cblacs_pinfo,
+                          // MPI_Init needs to be called here
 
     process_command_line_options(argc, argv);
 
@@ -106,37 +107,41 @@ int main(int argc, char ** argv){
 	Cblacs_gridinit(&ctx, "Row-Major", nprow, npcol);
 	Cblacs_gridinfo(ctx, &nprow, &npcol, &prow, &pcol); // prow, pcol are the row and col of the current process in the process grid
 
-    int M, N, mb_A, nb_A, mb_b, nb_b, mp, nq, lld, lld_distr, info, descA[9], descA_distr[9], descb[9], lwork, infoA, infoA_distr, infob, lld_b;
-    double FLOP;
+    int M, N, mb_A, nb_A, mb_b, nb_b, mp_A, nq_A, mp_b, nq_b, lld_A, lld_A_distr, lld_b, lld_b_distr;
+    int descA[9], descA_distr[9], descb[9], descb_distr[9], lwork, infoA, infoA_distr, infob, infob_distr, info;
     double *A;
     double *A_distr;
     double *b;
+    double *b_distr;
     struct data_points data;
 
     //Dimensions of global matrix
 	M=npoint;
 	N=nvar;
 
-	//Blocking factors (dimention of blocks) resp. for A and b (b is all in one block of dimensions M x 1)
-	mb_A = 10;
-	nb_A = 10;
-    mb_b = M;
+	//Blocking parameters (dimention of blocks) respectively for A and b (b is all in one block of dimensions M x 1)
+    //POSSIBLE WEAKNESS: do blocking parameters have to be related in some way? Is this a good way to set them?
+	mb_A = 1;
+	nb_A = 1;
+    mb_b = mb_A;    //POSSIBLE WEAKNESS: We want b to have the same number of rows as A_distr (Is this the way to do it?)
     nb_b = 1;
     
     if((prow == 0) && (pcol == 0)){
 
-        /********************* Build matrix A ********************************/	
+        /********************* Build matrices A and b ********************************/	
         long matrix_size = sizeof(double) * nvar * npoint;
-        char hsize[16];
-        human_format(hsize, matrix_size);
+        //char hsize[16];
+        //human_format(hsize, matrix_size);
         //printf("Matrix size: %sB\n", hsize);
 
         A = malloc(matrix_size);
-        if (A == NULL)
+        if (A == NULL){
             err(1, "cannot allocate matrix");
+        }            
 
         //printf("Reading data points from %s\n", data_filename);
         
+        //Build b
         load_data_points(data_filename, npoint, &data);
         b = malloc(npoint*sizeof(double));
         for(int i = 0; i< npoint; i++){
@@ -154,6 +159,7 @@ int main(int argc, char ** argv){
         struct spherical_harmonics model;
         setup_spherical_harmonics(lmax, &model);
 
+        //Build A
         for (int i = 0; i < npoint; i++) {
             computeP(&model, P, sin(data.phi[i]));
             
@@ -168,9 +174,16 @@ int main(int argc, char ** argv){
                 }
             }
         }
-        FLOP = 2. * nvar * nvar * npoint;
-        char hflop[16];
-        human_format(hflop, FLOP);
+        //printf("Built matrices A, b\n");
+        //DEBUG
+        printf("\nProcess [%d, %d] printing A:\n", prow, pcol);
+        for(int i=0; i<N*M; i++){
+            //A[i]=i;
+            printf("%d:\t%f\n", i, A[i]);
+        }
+        //FLOP = 2. * nvar * nvar * npoint;
+        //char hflop[16];
+        //human_format(hflop, FLOP);
         //printf("Least Squares (%sFLOP)\n", hflop);
         /***********************************************************************/
 	}
@@ -179,64 +192,92 @@ int main(int argc, char ** argv){
         b = NULL;    
     }
 	
-	double start = wtime();  
+	//double start = wtime();  
 	
     /**************************** SCALAPACK CALL *********************************/
 
-	//Compute the size of the local part of the matrix A, called A_distr	
-	mp = numroc_(&M, &mb_A, &prow, &i_zero, &nprow);
-	nq = numroc_(&N, &nb_A, &pcol, &i_zero, &npcol);
-
-	A_distr = malloc(mp*nq*sizeof(double));
+	//Compute the size of the local part of the matrices A and b, called A_distr and b_distr	
+    //POSSIBLE DEFECT: the last arguments might have to be &nprocs, and not &nprows, &npcols. Cf. docs
+	mp_A = numroc_(&M, &mb_A, &prow, &i_zero, &nprow);
+	nq_A = numroc_(&N, &nb_A, &pcol, &i_zero, &npcol);
+    mp_b = mp_A; //numroc_(&M, &mb_b, &prow, &i_zero, &nprow);
+    nq_b = 1; //numroc_(&i_one, &nb_b, &pcol, &i_zero, &npcol);
+    
+	A_distr = malloc(mp_A*nq_A*sizeof(double));
 	if(A_distr == NULL){
 		err(1, "Couldn't allocate A_distr \n");
-	}	
+	}
+
+    b_distr = malloc(mp_b*nq_b*sizeof(double));
+    if(b_distr == NULL){
+        err(1, "Could not allocate b_distr");
+    }	
 
 	//Initialize the descriptors of A, A_distr, b
-	lld = MAX( numroc_( &M, &M, &prow, &i_zero, &nprow ), 1 );
-    lld_distr = MAX(numroc_(&M, &mb_A, &prow, &i_zero, &nprow), 1);
-    lld_b = MAX(numroc_(&M, &mb_b, &prow, &i_zero, &nprow), 1); 
-	descinit_(descA, &M, &N, &M, &N, &i_zero, &i_zero, &ctx, &lld, &infoA);
-	descinit_(descA_distr, &M, &N, &mb_A, &nb_A, &i_zero, &i_zero, &ctx, &lld_distr, &infoA_distr);
-    descinit_(descb, &M, &i_one, &mb_b, &nb_b, &i_zero, &i_zero, &ctx, &lld_b, &infob);
+	lld_A = MAX( numroc_( &M, &M, &prow, &i_zero, &nprow ), 1 );
+    lld_A_distr = MAX(mp_A, 1);
+    lld_b = MAX(numroc_(&M, &M, &prow, &i_zero, &nprow), 1); 
+    lld_b_distr = MAX(mp_b, 1);
+	descinit_(descA, &M, &N, &M, &N, &i_zero, &i_zero, &ctx, &lld_A, &infoA);
+	descinit_(descA_distr, &M, &N, &mb_A, &nb_A, &i_zero, &i_zero, &ctx, &lld_A_distr, &infoA_distr);
+    descinit_(descb, &M, &i_one, &M, &i_one, &i_zero, &i_zero, &ctx, &lld_b, &infob);
+    descinit_(descb_distr, &M, &i_one, &mb_b, &nb_b, &i_zero, &i_zero, &ctx, &lld_b_distr, &infob_distr);
+    //printf("Process [%d, %d] initialized its descriptors\n", prow, pcol);
     //DEBUG
     /*
     for(int k = 0; k<9; k++){
         printf("%d: %d\n", k, descb[k]);
     }
     */
-    if((infoA != 0) || (infoA_distr != 0) || (infob != 0)){
+    if((infoA != 0) || (infoA_distr != 0) || (infob != 0) || (infob_distr != 0)){
         err(1, "Could not initialize descriptors \n");
     }
 
-    //Distribute the matrix
-    //TODO: ia, ja might have to be changed, this looks wrong intuitively
+    //Distribute matrices A and b
+    //POSSIBLE DEFECT: ia, ja, ib, jb might have to be changed, this looks wrong intuitively. 
     int ia, ja, ib, jb;
     ia = i_one;
     ja = i_one;
     ib = i_one;
     jb = i_one;
-    pdgemr2d_(&mp, &nq, A, &ia, &ja, descA, A_distr, &i_one, &i_one, descA_distr, &ctx); //parameters 3,4 don't really make sense to me but they seem to work
+    //POSSIBLE DEFECT: if nothing else works, probably these are not being copied right. Check the docs about the "context" thing
+    pdgemr2d_(&mp_A, &nq_A, A, &ia, &ja, descA, A_distr, &i_one, &i_one, descA_distr, &ctx); 
+    //printf("Process [%d, %d] has its A_distr\n", prow, pcol);
+    pdgemr2d_(&mp_b, &nq_b, b, &ib, &jb, descb, b_distr, &i_one, &i_one, descb_distr, &ctx);
+    //printf("Process [%d, %d] has its b_distr\n", prow, pcol);
+
+    //DEBUG
+    printf("\nProcess [%d, %d] printing A_distr:\n", prow, pcol);
+    for(int i=0; i<mp_A*nq_A; i++){
+        printf("%d:\t%f\n", i, A_distr[i]);
+    }
 
     //Call the ScaLAPACK least-square-solution routine. The first call is a query to get the optimal value of lwork
+    //POSSIBLE DEFECT: ia, ja, ib, jb might have to be changed. Doing this above will change them here as well, this comment is just to notice that those
+    //                 parameters are used here as well
     double temp_work[1];
-    pdgels_("N", &mp, &nq, &i_one, A_distr, &ia, &ja, descA_distr, b, &ib, &jb, descb, temp_work, &i_neg_one, &info);
-    printf("INFO: %d", info);
+    pdgels_("N", &mp_A, &nq_A, &i_one, A_distr, &ia, &ja, descA_distr, b_distr, &ib, &jb, descb_distr, temp_work, &i_neg_one, &info);
     if(info!=0){
         err(1, "Query call of pdgels failed \n");
     }
+    //printf("Process [%d, %d] finished query PDGELS call\n", prow, pcol);
     lwork = temp_work[0];
     double *work = malloc(lwork * sizeof(double));
 
-    pdgels_("N", &mp, &nq, &i_one, A_distr, &ia, &ja, descA_distr, b, &ib, &jb, descb, work, &i_neg_one, &info);
+    pdgels_("N", &mp_A, &nq_A, &i_one, A_distr, &ia, &ja, descA_distr, b_distr, &ib, &jb, descb_distr, work, &i_neg_one, &info);
     if(info!=0){
         err(1, "Call of pdgels failed \n");
     }
+    //printf("Process [%d, %d] finished actual PDGELS call\n", prow, pcol);
 
     //Copy the local results into the global matrix
-    pdgemr2d_(&mp, &nq, A_distr, &i_one, &i_one, descA_distr, A, &ia, &ja, descA, &ctx); //parameters 7,8 don't really make sense to me but they seem to work
+    //POSSIBLE DEFECT: again ia, ja, ib, jb. See above
+    pdgemr2d_(&mp_A, &nq_A, A_distr, &i_one, &i_one, descA_distr, A, &ia, &ja, descA, &ctx); 
+    pdgemr2d_(&mp_b, &nq_b, b_distr, &i_one, &i_one, descb_distr, b, &ib, &jb, descb, &ctx);
+    //printf("Process [%d, %d] copied the local result into global matrix\n", prow, pcol);
 
     free(A_distr);
+    free(b_distr);
 
     /****************************************************************************/
 	
@@ -244,12 +285,14 @@ int main(int argc, char ** argv){
 
     //Save the model in the output file
     if((prow == 0 ) && (pcol == 0 )){
+        /*
         FLOP = 1; //DEBUG
         double t = wtime()  - start;
         double FLOPS = FLOP / t;	
         char hflops[16];
         human_format(hflops, FLOPS);
         //printf("Completed in %.1f s (%s FLOPS)\n", t, hflops);
+        */
         double res = 0;
         for (int j = nvar; j < npoint; j++){
             res += data.V[j] * data.V[j];
@@ -270,6 +313,5 @@ int main(int argc, char ** argv){
 
     //Exit process grid. This also finalizes MPI	
 	Cblacs_gridexit(ctx);
-	Cblacs_exit(i_zero);
-    //MPI_Finalize();	
+	Cblacs_exit(i_zero);    //This also calls MPI_finalize if the argument is 0
 }
