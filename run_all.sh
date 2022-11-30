8115319df3d7a1ba7b7a1135373d799dd865b0f6
marcoparadina <mparadina10@gmail.com>
2022-11-30 10:52:21 +0100

    final commit before submission

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,10 @@
 !#/bin/bash
 
-for host in $(cat "$OAR_NODEFILE" | uniq)
+for host in $(cat "$OAR_NODEFILE" | uniq);
 do
     oarsh "$host" sudo-g5k apt install libscalapack-openmpi-dev -y &
-done
-cd PPAR_project1
+done;
+
 make
+
 mpirun -n 961 -machinefile "$OAR_NODEFILE" ./pmodel3_clean --data /srv/storage/ppar@storage1.nancy.grid5000.fr/ETOPO1_ultra.csv --model out.csv --npoint 120000 --lmax 124
